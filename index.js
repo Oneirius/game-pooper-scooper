8fce423d1363162906d89b3f99cefd073098b252
Kumar Daryanani <destral@msn.com>
2024-01-29 11:56:08 +0100

    implement player movement

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ gameBoardElement = document.querySelector("#game-board");
 
 // Create player entity
 const player = new Player(384, 308);
+
+
+requestAnimationFrame(gameLoop);
+
+
+function gameLoop() {
+    player.move();
+    //console.log("Brain");
+    requestAnimationFrame(gameLoop);
+}
